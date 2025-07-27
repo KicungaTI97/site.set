ad31b73a46052baa819ace8c9cc7d2b16828635d
kicungati97 <kicungati@gmail.com>
2025-07-27 13:45:21 +0100

    Construção do Blgo - finalizando com a pagina de listagem de post e incrementado a funcionalidade de busca, filtrando os post

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,14 +1,37 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
+import { PostCard } from "./components/post-card/post-card";
+import { PostGridCard } from "./components/post-grid-card";
+import { allPosts } from "contentlayer/generated";
+import { Inbox } from "lucide-react";
+
+function highlightText(text: string, query: string) {
+  if (!query) return text;
+  
+  const regex = new RegExp(`(${query.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})`, 'gi');
+  return text.replace(regex, '<mark class="bg-yellow-200 text-gray-900 px-1 rounded ">$1</mark>');
+}
+
 
 export function BlogList() {
-  const router = useRouter();
-  const query = router.query.q as string;
-  const pageTitle = query ? `Resultado de busca "${query}"` : "Dicas e estratégias para impulsionar seu negócio";
+	const router = useRouter();
+	const query = router.query.q as string;
+	const pageTitle = query
+		? `Resultado de busca "${query}"`
+		: "Dicas e estratégias para impulsionar seu negócio";
 
+	const posts = query
+		? allPosts.filter(
+				(post) =>
+					post.title.toLowerCase().includes(query.toLowerCase()) ||
+					post.description.toLowerCase().includes(query.toLowerCase()),
+			)
+		: allPosts;
+
+	const hasPosts = posts.length > 0;
 	return (
 		<div className="flex flex-col py-24 flex-grow h-full">
-			<header>
+			<header className="pb-14">
 				<div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
 					<div className="flex flex-col gap-4 md:px-0">
 						{/*TAG*/}
@@ -25,6 +48,42 @@ export function BlogList() {
 					<Search />
 				</div>
 			</header>
+
+			{/*Listagem de post */}
+			{hasPosts && (
+				<PostGridCard>
+					{posts.map((post) => (
+						<PostCard
+							key={post._id}
+							title={post.title}
+							description={post.description}
+              heighlightTitle={query ? highlightText(post.title, query) : undefined}
+              heighlightDescription={query ? highlightText(post.description, query) : undefined}
+							image={post.image}
+							data={new Date(post.date).toLocaleDateString("pt-AO")}
+							slug={post.slug}
+							author={{
+								avatar: post.author?.avatar,
+								name: post.author?.name,
+							}}
+						/>
+					))}
+				</PostGridCard>
+			)}
+      {!hasPosts && (
+        <div className="container px-8">
+        <div className="flex flex-col items-center justify-center h-full rounded-md border border-dashed border-gray-300 p-8 md:p-12">
+          {/* No posts found message */}
+          <Inbox className="w-12 h-12 text-cyan-100 mb-4" />
+          <h2 className="text-heading-md text-gray-100">
+            Nenhum post encontrado
+          </h2>
+          <p className="text-body-sm text-gray-300">
+            Tente ajustar sua busca ou volte mais tarde.
+          </p>
+        </div>          
+        </div>
+      )}
 		</div>
 	);
 }
---
+++ src/templates/blog/components/post-card/post-card.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type Author = {
+  name: string;
+  avatar: string;
+}
+type PostCardProps = {
+  slug: string;
+  title: string;
+  description: string;
+  image: string;
+  data: string;
+  author: Author;
+
+  heighlightTitle?: string;
+  heighlightDescription?: string;
+
+
+}
+export const PostCard = ({
+  data,
+  author,
+  description,
+  image,
+  slug,
+  title,
+  heighlightTitle,
+  heighlightDescription
+
+}:PostCardProps) => {
+	return (
+		<Link
+			href={`/blog/${slug}`}
+			className="w-full max-w-2xl rounded-[12px] border-[1px] border-gray-400 bg-gray-600 overflow-hidden transition-all duration-300 hover:border-[1px] hover:border-blue-300"
+		>
+			{/*Post Content */}
+			<div className="p-2 rounded-md overflow-hidden ">
+				{/*Image Container */}
+				<div className="relative">
+
+          {/* Date Container */}
+					<div className="absolute top-0 right-0 px-3 py-1 bg-gray-600 backdrop-blur-sm rounded-bl-[10px]">
+						<span className="text-body-xs text-gray-300">{data}</span>
+					</div>
+					<Image
+						src={image}
+						alt={title}
+						width={288}
+						height={144}
+						className="w-full h-40 object-cover object-center rounded-t-[8px]"
+					/>
+				</div>
+
+				{/*Post info */}
+				<div className="px-2 mt-4 space-y-4">
+					{heighlightTitle ? (
+            <h2 className="text-heading-sm text-gray-100 line-clamp-3" dangerouslySetInnerHTML={{ __html: heighlightTitle}} />
+          ) : (
+            <h2 className="text-heading-sm text-gray-100 line-clamp-3">{title}</h2>
+          )}
+
+					{heighlightDescription ? (
+            <p className="text-gray-300 text-body-sm line-clamp-3" dangerouslySetInnerHTML={{ __html: heighlightDescription}} />
+          ) : (
+            <p className="text-gray-300 text-body-sm line-clamp-3">
+						{description}
+					</p>
+          )}
+
+					{/*Post footer */}
+					<div className="flex items-center gap-3 border-t border-gray-400 py-4">
+						<div className="relative h-5 w-5 md:h-6 md:w-6 overflow-hidden rounded-full border-blue-200 border-[1px]">
+							<Image
+								src={author?.avatar}
+								alt={title}
+								fill
+								className="object-cover rounded-md"
+							/>
+						</div>
+            <span className="text-body-sm text-gray-300">{author?.name}</span>
+					</div>
+				</div>
+			</div>
+		</Link>
+	);
+};
