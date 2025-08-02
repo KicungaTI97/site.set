d0a34a7dd90d847452eab35a26d1c0ceb530a33e
kicungati97 <kicungati@gmail.com>
2025-08-02 15:37:34 +0100

    Adiciona metadados e tags OpenGraph para otimização de SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
+import type { Metadata } from "next";
 import { LandingPage } from "@/templates/landing-page";
 
+export const metadata: Metadata = {
+  title: "Site.Set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: "Site.Set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://site-set-4j7z.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt-AO",
+    type: "website",
+    images: [
+      {
+        url: "https://site-set-4j7z.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set - Venda seus produtos como afiliado em um único lugar",
+      },
+    ],
+    
+  },
+
+};
 export default function HomePage() {
   return <LandingPage />;
 }
